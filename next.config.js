828dca854f7c3bd84ec42aec486e99742052421e
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-10-14 12:19:49 -0300

    usando FormData para capturar campos da tag form

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig
+
+// Ao inves de usar o Babel pra transpilar de JS para TS, usar a transcrição do experimental
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  reactStrictMode: false,
   swcMinify: true,
-}
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,18 @@
-import { Typography } from '@mui/material';
-import React from 'react'
+import React, { useEffect, useState, FormEvent } from 'react'
+import {
+	Typography,
+    ThemeProvider,
+    createTheme,
+    Container,
+    CssBaseline,
+    Box,
+    TextField,
+	FormControlLabel,
+	Checkbox,
+	Button,
+	Snackbar,
+} from '@mui/material';
 import Link from 'next/link';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Container from '@mui/material/Container';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 
 
 type CopyProps = {
@@ -16,10 +23,10 @@ type CopyProps = {
 function Copyright(props: CopyProps) {
     return (
         <Typography>
-            {'Copyright ©️ '}
+            {'Copyright ©️ - '}
             <Link color='inherit' href={props.site}>
                 {props.site} 
-            </Link> {' '}
+            </Link> {' - '}
             { new Date().getFullYear() }
             { '.' }
         </Typography>
@@ -29,25 +36,86 @@ function Copyright(props: CopyProps) {
 const theme = createTheme();
 
 export default function LoginPage() {
-  return (
-    <ThemeProvider theme={theme}>
-        <Container>
-            <CssBaseline />
-                <Box sx={{ mt: 8, display: 'flex', flexDirection: 'column', alignItens: 'center' }}>
-                    <Typography component='h1' variant='h5'>
-                        Login
-                    </Typography>
-                </Box>
-                <Box component='form' onSubmit={(e) => {console.log('OK! Enviou.')}}>
-                    <TextField margin='normal' required fullWidth id='email' label='Digite o e-mail' name='email' autoComplete='email' autoFocus />
-                    <TextField margin='normal' required fullWidth id='password' type='password' label='Digite a senha' name='password' autoComplete='current-password' autoFocus />
-                </Box>
-        </Container>
-        <div>Login Page - C6 bank</div>
-            
-        <Copyright site='www.avanade.com.br'></Copyright>
 
-    </ThemeProvider>
+	const [contador, setContador] = useState(5);
+	const [name, setName] = useState('');
+	const [error, setError] = useState<boolean>('');
+	const [errorMessage, setErrorMessage] = useState('');
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState('');
+
+
+	useEffect(()=>{
+		if (contador == 0) {
+			document.title = `Executando useEffect a primeira vez = ${contador}`;
+		} else {
+			document.title = `Executando useEffect a cada alteração = ${contador}`;	
+		}
+		console.log(`a cada chamada ${contador}`);
+	}, [])
+
+	// useEffect(() => {
+	// 	setName(name);
+	// 	console.log(name);
+	// }, [name]);
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault(); // previne o formulario enviar o form (recarregar a página)
+		const data = new FormData(event.currentTarget); // captura todos os campos da tag Form
+		console.log(data.get('email'));
+		setName(data.get('email'));
+		setPassword(data.get('value'));
+	};
+	
+	useEffect(() => {
+		if (!password || password.length < 6) {
+			setError(true);
+			setErrorMessage('senha precisa ter no mínimo 6 digitos')
+		} else {
+			setError(false);
+
+		}
+	}, [password]);
+
+	useEffect(() => {
+		if (!error) {
+			setErrorMessage('')
+		}
+	}, [error])
+	
+	
+	return (
+	<ThemeProvider theme={theme}>
+			<Container>
+					<CssBaseline />
+							<Box sx={{ mt: 8, display: 'flex', flexDirection: 'column', alignItens: 'center' }}>
+									<Typography component='h1' variant='h5'>
+											Login
+									</Typography>
+							</Box>
+
+								{`O state contador vale ${contador}`}
+								<button onClick={() => setContador(contador + 1)}>Muda o Contador</button>
+
+							<Box component='form' onSubmit={handleSubmit}>
+									<TextField margin='normal' required fullWidth id='email' label='Digite o e-mail' name='email' autoComplete='email' autoFocus />
+									<TextField margin='normal' required fullWidth id='password' type='password' label='Digite a senha' name='password' autoComplete='current-password' autoFocus />
+									<FormControlLabel control={<Checkbox value='remember' color='primary' />} label='lembrar de mim' />
+									<Button
+										type='submit'
+										fullWidth 
+										variant='contained'
+										sx={{mt: 3, mb: 2}}
+									>
+										Entrar
+									</Button>
+							</Box>
+			</Container>
+			<div>Login Page - C6 bank</div>
+					
+			<Copyright site='www.avanade.com.br'></Copyright>
+
+	</ThemeProvider>
   )
 };
 
